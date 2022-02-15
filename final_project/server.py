3ff5907c64b46d65cc01c955e2fafed29e94caa6
swapnil shukla <swapnil.shukla1508@gmail.com>
2022-02-15 13:36:23 +0530

    Final commit

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,23 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
-
-app = Flask("Web Translator")
+app = Flask("Web Translator",template_folder='templates')
 
 @app.route("/englishToSpanish")
 def englishToSpanish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to Spanish"
+    return translator.englishToFrench(textToTranslate)
 
 @app.route("/spanishToEnglish")
 def spanishToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    return translator.frenchToEnglish(textToTranslate)
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
